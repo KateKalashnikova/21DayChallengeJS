1fc8096835f7f56b7ae32e92c48f8c2b6afe9473
Kate Kalashnikova <kkalashnikova@kruxanalytics.com>
2021-11-22 10:55:28 -0700

    first day challenge

---
+++ script.js
@@ -0,0 +1,11 @@
+// 21-Day Coding Challenge
+//Day 1
+const parseMessage = (origin, message) => {
+
+    var result
+    result = origin + ':' + ' ' + message;
+    return result;
+
+}
+const message = "Hello";
+const origin = "MC";
