0ee1fa831b39cd279ff4b7303a50417a7a84dc0e
Kate Kalashnikova <kkalashnikova@kruxanalytics.com>
2021-11-23 17:22:43 -0700

    second day task

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ const parseMessage = (origin, message) => {
 }
 const message = "Hello";
 const origin = "MC";
+
+//Day 2
+//given example
+const exampleAstronaut = {
+    firstName: "Yuri",
+    lastName: "Gagarin",
+    nickname: "First!",
+    prefix: "Cosmonaut"
+  }
+
+const generateAstronautTag = (astronaut) => {
+  
+const astronautOutput = (`${astronaut.prefix}: ${astronaut.firstName} "${astronaut.nickname}" ${astronaut.lastName}`)
+
+    return astronautOutput 
+}
+
+  
